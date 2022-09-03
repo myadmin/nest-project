ee246af33b2be985d4b6303a70c72d285d6f4dc4
zhouwei <zhou21192345@163.com>
2022-09-03 23:27:13 +0800

    feat: 更新连接数据库信息

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,11 +20,11 @@ import envConfig from '../config/env';
             useFactory: async (configService: ConfigService) => ({
                 type: 'mysql', // 数据库类型
                 entities: [PostsEntity], // 数据表实体
-                host: configService.get('DB_HOST', 'localhost'), // 主机，默认为localhost
-                port: configService.get<number>('DB_PORT', 3306), // 端口号
-                username: configService.get('DB_USER', 'root'), // 用户名
-                password: configService.get('DB_PASSWORD', '123456'), // 密码
-                database: configService.get('DB_DATABASE', 'node'), //数据库名
+                host: configService.get('DB_HOST'), // 主机，默认为localhost
+                port: configService.get<number>('DB_PORT'), // 端口号
+                username: configService.get('DB_USER'), // 用户名
+                password: configService.get('DB_PASSWORD'), // 密码
+                database: configService.get('DB_DATABASE'), //数据库名
                 timezone: '+08:00', //服务器上配置的时区
                 synchronize: false, //根据实体自动创建数据库表， 生产环境建议关闭
             }),
